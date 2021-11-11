95f25298f8c5e0bcea4dec63058036b3413a8a57
Patrick Lecuyer <patl@patl.io>
2021-11-10 20:35:50 -0500

    feat: added propertyinspector

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -3,6 +3,7 @@ import { Streamdeck } from '@rweich/streamdeck-ts';
 const plugin = new Streamdeck().plugin();
 
 // your code here..
+
 plugin.on('willAppear', ({ context }) => {
   plugin.setTitle('test', context);
 });
